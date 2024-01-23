3e63ecaffd6317ef490e9218bc1107c7b7a87a2d
Ricky Ponting <qarimahdi217@gmail.com>
2024-01-23 22:49:04 +0600

    Implement CRUD functionalities for bookings collection.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 // mongodb driver
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // const uri = "mongodb+srv://<username>:<password>@cluster0.geunt7i.mongodb.net/?retryWrites=true&w=majority";
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.geunt7i.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -25,21 +25,55 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        
-                const database = client.db("HouseHunterDB");
-                const seed = database.collection("seed");
 
+        const database = client.db("HouseHunterDB");
+        const seed = database.collection("seed");
+        const bookingCollection = client.db("HouseHunterDB").collection("bookings");
+
+
+        // seed
         // localhost:5000/api/v1/seed?page=5&limit=10
         app.get("/api/v1/seed", async (req, res) => {
 
             //pagination
-            const page = +req.query.page; 
+            const page = +req.query.page;
             const limit = +req.query.limit;
-            const skip = (page -1) * limit || 5;
+            const skip = (page - 1) * limit || 5;
 
             const cursor = seed.find().skip(skip).limit(limit);
             const result = await cursor.toArray();
 
+            res.send(result);
+        })
+
+        // booking 
+        // user (house renter dashboard)
+        app.get("/api/v1/user/bookings", async (req, res) => {
+            const cursor = bookingCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.post("/api/v1/user/bookings", async (req, res) => {
+            const doc = req.body;
+            const result = await bookingCollection.insertOne(doc);
+            res.send(result);
+        })
+
+        app.delete("/api/v1/user/bookings/:id", async (req, res) => {
+            const { id } = req.params;
+            const query = { _id: new ObjectId(id) };
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        app.patch("/api/v1/user/bookings/:id", async (req, res) => {
+            const { id } = req.params;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: req.body
+            };
+            const result = await bookingCollection.updateOne(filter, updateDoc);
             res.send(result);
         })
 
